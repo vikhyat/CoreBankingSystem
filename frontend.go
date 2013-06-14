021a89f1cf88e38b92a71685f69c6b877cbef834
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-14 11:06:37 +0530

    Redis!

--- frontend.go
+++ frontend.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"log"
 	"net/http"
 )
+
+var redisConnection redis.Conn
+var redisErr error
 
 func depositHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
 }
 
 func main() {
-	http.HandleFunc("/deposit", depositHandler)
+	redisConnection, redisErr = redis.Dial("tcp", ":6379")
+	if redisErr != nil {
+		log.Fatal(redisErr)
+	}
 
+	http.HandleFunc("/deposit", depositHandler)
 	log.Fatal(http.ListenAndServe(":8341", nil))
 }
