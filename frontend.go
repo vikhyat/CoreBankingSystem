fc0119aebd98fecff52a90e917ba8f04ab5653da
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-06-14 12:03:48 +0530

    Working "withdraw" functionality.

--- frontend.go
+++ frontend.go
@@ -15,6 +15,19 @@ func redisConnection(account int) redis.Conn {
 	return redisConnectionShards
 }
 
+func accountKey(account int) string {
+	return fmt.Sprintf("account:%d", account)
+}
+
+func getBalance(account int) int {
+	redisConnection(account).Do("SETNX", accountKey(account), 0)
+	balance, err := redis.Int(redisConnection(account).Do("GET", accountKey(account)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return balance
+}
+
 func depositHandler(w http.ResponseWriter, r *http.Request) {
 	account, err := strconv.Atoi(r.FormValue("account"))
 	if err != nil {
@@ -26,20 +39,29 @@ func depositHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	accountKey := fmt.Sprintf("account:%d", account)
+	redisConnection(account).Do("SETNX", accountKey(account), 0)
+	redisConnection(account).Do("INCRBY", accountKey(account), amount)
 
-	redisConnection(account).Do("SETNX", accountKey, 0)
-	redisConnection(account).Do("INCRBY", accountKey, amount)
-	reply, err := redis.Int(redisConnection(account).Do("GET", accountKey))
+	fmt.Fprintf(w, "{account: %d, balance: %d}", account, getBalance(account))
+}
+
+func withdrawHandler(w http.ResponseWriter, r *http.Request) {
+	account, err := strconv.Atoi(r.FormValue("account"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintf(w, "{account: %d, balance: %d}", account, reply)
-}
+	amount, err := strconv.Atoi(r.FormValue("amount"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func withdrawHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
+	if getBalance(account) >= amount {
+		redisConnection(account).Do("DECRBY", accountKey(account), amount)
+		fmt.Fprintf(w, "{account: %d, balance: %d}", account, getBalance(account))
+	} else {
+		fmt.Fprintf(w, "{error: \"insufficient funds\"}")
+	}
 }
 
 func main() {
